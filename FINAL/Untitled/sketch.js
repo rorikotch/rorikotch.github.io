59af63ecb7299aa8ab65a3b1f2903747529607ad
Rori Kotch <r.kotch1@umiami.edu>
2016-12-11 18:51:21 -0500

    Deleted word from string

--- FINAL/Untitled/sketch.js
+++ FINAL/Untitled/sketch.js
@@ -39,7 +39,7 @@ function showResult() {
       for (var word in currentResult) {
          // console.log(currentResult[word]);
         
-      if (currentResult[word] == "hello" , "hi") {
+      if (currentResult[word] == "hello") {
           var foo = new p5.Speech();
           foo.speak("Hi there");
           //write it to do something here 
