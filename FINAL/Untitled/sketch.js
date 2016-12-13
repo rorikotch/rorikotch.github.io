f9b34875e1579a6e550b76e599d25e498f3c6b77
Rori Kotch <r.kotch1@umiami.edu>
2016-12-13 13:37:53 -0500

    Edited phrase

--- FINAL/Untitled/sketch.js
+++ FINAL/Untitled/sketch.js
@@ -74,8 +74,8 @@ function showResult() {
             if (currentResult[word] == "cats"){
               foo.speak("They're the best");
           }
-            if (currentResult[word] == "guacamole"){
-              foo.speak("avos are my favorite");
+            if (currentResult[word] == "avocado toast"){
+              foo.speak("avo toast is my favorite");
           }
             if (currentResult[word] == "drunk"){
               foo.speak("last night I was so ratchet");
