5dc0b4bae3a8a08c257f97360d3757d827667806
Rori Kotch <r.kotch1@umiami.edu>
2016-12-12 22:28:14 -0500

    Reverted to old code

--- FINAL/Untitled/sketch.js
+++ FINAL/Untitled/sketch.js
@@ -2,7 +2,6 @@ var myRec = new p5.SpeechRec();
 var pTag = document.createElement("p");
 var myP;
 var allMessage = "";
-var foo;
 
 
 // sets myRec to listen continuously
@@ -11,7 +10,7 @@ myRec.continuous = true;
 myRec.interimResults = true;
 
 function setup() {
-    foo = new p5.Speech();
+  var foo = new p5.Speech();
   foo.speak("Speak now");
   myRec.onResult = showResult;
   myRec.start();
@@ -36,27 +35,31 @@ function showResult() {
       
       //the easiest way to try to loop through it would be for word in ..
       
-      var currentResult = myRec.resultString;
-      if (currentResult == "How are you"){
-        foo.speak("I am well, how are you"); 
-      } else {
-        
-      for (var word in currentResult.split(" ")) {
+      var currentResult = myRec.resultString.split(" ");
+      for (var word in currentResult) {
          // console.log(currentResult[word]);
-          
+        
       if (currentResult[word] == "hello") {
+          var foo = new p5.Speech();
           foo.speak("Hi there");
       }
-      } 
+      
       }
     
-      //when you are more comfoprtable add text prompts for the user in the setup look at examples from class
+    
       
       
     //pTag.innerHTML = myRec.resultString;
       allMessage = allMessage + " " + myRec.resultString;
       myP.html(allMessage);
       
+    
+
+    // <p>TriStar is a scary computer.</p>
+
+    // Now find where you want to add the new p.
+    // Get the element with the id "transcript"
+    // Then add the p tag created above.
 
     //var transcript = document.getElementById("transcript");
     //transcript.appendChild(pTag);
