c94a790522b5ef48d68de9300dd652f6b373fc05
Rori Kotch <r.kotch1@umiami.edu>
2017-04-29 17:42:47 -0400

    Trying to fix

--- script2.js
+++ script2.js
@@ -69,67 +69,67 @@ Highcharts.chart("chart", {
     }]
   });
 
-  // /////
-  // $.getJSON('http://cheeseproject.me/data.html?', function (data) {
-  //
-  //     // Make codes uppercase to match the map data
-  //     $.each(data, function () {
-  //         this.code = this.code.toUpperCase();
-  //     });
-  //
-  //     // Instanciate the map
-  //     Highcharts.mapChart('map', {
-  //
-  //         chart: {
-  //             borderWidth: 1
-  //         },
-  //
-  //         title: {
-  //             text: 'Cheese Producing States by Thousands of Cows'
-  //         },
-  //
-  //         legend: {
-  //             layout: 'horizontal',
-  //             borderWidth: 0,
-  //             backgroundColor: 'rgba(255,255,255,0.85)',
-  //             floating: true,
-  //             verticalAlign: 'top',
-  //             y: 25
-  //         },
-  //
-  //         mapNavigation: {
-  //             enabled: true
-  //         },
-  //
-  //         colorAxis: {
-  //             min: 1,
-  //             type: 'logarithmic',
-  //             minColor: '#EEEEFF',
-  //             maxColor: '#000022',
-  //             stops: [
-  //                 [0, '#EFEFFF'],
-  //                 [0.67, '#4444FF'],
-  //                 [1, '#000022']
-  //             ]
-  //         },
-  //
-  //         series: [{
-  //             animation: {
-  //                 duration: 1000
-  //             },
-  //             data: data,
-  //             mapData: Highcharts.maps['countries/us/us-all'],
-  //             joinBy: ['postal-code', 'code'],
-  //             dataLabels: {
-  //                 enabled: true,
-  //                 color: '#FFFFFF',
-  //                 format: '{point.code}'
-  //             },
-  //             name: 'Population density',
-  //             tooltip: {
-  //                 pointFormat: '{point.code}: {point.value}/km²'
-  //             }
-  //         }]
-  //     });
-  // });
-  // //////
+  /////
+  $.getJSON('http://cheeseproject.me/data.html?', function (data) {
+
+      // Make codes uppercase to match the map data
+      $.each(data, function () {
+          this.code = this.code.toUpperCase();
+      });
+
+      // Instanciate the map
+      Highcharts.mapChart('map', {
+
+          chart: {
+              borderWidth: 1
+          },
+
+          title: {
+              text: 'Cheese Producing States by Thousands of Cows'
+          },
+
+          legend: {
+              layout: 'horizontal',
+              borderWidth: 0,
+              backgroundColor: 'rgba(255,255,255,0.85)',
+              floating: true,
+              verticalAlign: 'top',
+              y: 25
+          },
+
+          mapNavigation: {
+              enabled: true
+          },
+
+          colorAxis: {
+              min: 1,
+              type: 'logarithmic',
+              minColor: '#EEEEFF',
+              maxColor: '#000022',
+              stops: [
+                  [0, '#EFEFFF'],
+                  [0.67, '#4444FF'],
+                  [1, '#000022']
+              ]
+          },
+
+          series: [{
+              animation: {
+                  duration: 1000
+              },
+              data: data,
+              mapData: Highcharts.maps['countries/us/us-all'],
+              joinBy: ['postal-code', 'code'],
+              dataLabels: {
+                  enabled: true,
+                  color: '#FFFFFF',
+                  format: '{point.code}'
+              },
+              name: 'Population density',
+              tooltip: {
+                  pointFormat: '{point.code}: {point.value}/km²'
+              }
+          }]
+      });
+  });
+  //////
