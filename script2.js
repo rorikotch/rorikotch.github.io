a58a4995ee3eea9e02dc48801956770b02df24c3
Rori Kotch <r.kotch1@umiami.edu>
2017-04-29 13:32:02 -0400

    Noted out chart 2

--- script2.js
+++ script2.js
@@ -34,67 +34,67 @@ Highcharts.chart("containerfour", {
     }]
   });
 
-
-$.getJSON('data.json', function(data) {
-
-    // Make codes uppercase to match the map data
-    $.each(data, function () {
-        this.code = this.code.toUpperCase();
-    });
-
-  Highcharts.mapChart('containerfive', {
-
-          chart: {
-              borderWidth: 1,
-              renderTo: 'containerfive',
-              type: 'map'
-          },
-
-          title: {
-              text: 'US Milk Producing States by Population of Dairy Cows in Thousands'
-          },
-
-          legend: {
-              layout: 'horizontal',
-              borderWidth: 0,
-              backgroundColor: 'rgba(255,255,255,0.85)',
-              floating: true,
-              verticalAlign: 'top',
-              y: 25
-          },
-
-          mapNavigation: {
-              enabled: true
-          },
-
-          colorAxis: {
-              min: 1,
-              type: 'logarithmic',
-              minColor: '#EEEEFF',
-              maxColor: '#000022',
-              stops: [
-                  [0, '#EFEFFF'],
-                  [0.67, '#4444FF'],
-                  [1, '#000022']
-              ]
-          },
-
-          series: [{
-              animation: {
-                  duration: 1000
-              },
-              data: data,
-              mapData: Highcharts.maps['countries/us/us-all'],
-              joinBy: ['postal-code', 'code'],
-              dataLabels: {
-                  enabled: true,
-                  color: '#FFFFFF',
-                  format: '{point.code}'
-              },
-              name: 'dairy cow population in thousands',
-              tooltip: {
-                  pointFormat: '{point.code}: {point.value}'
-              }
-          }]
-      });
-    });
+// COULDN'T GET THIS TO WORK
+// $.getJSON('data.json', function(data) {
+//
+//     // Make codes uppercase to match the map data
+//     $.each(data, function () {
+//         this.code = this.code.toUpperCase();
+//     });
+//
+//   Highcharts.mapChart('containerfive', {
+//
+//           chart: {
+//               borderWidth: 1,
+//               renderTo: 'containerfive',
+//               type: 'map'
+//           },
+//
+//           title: {
+//               text: 'US Milk Producing States by Population of Dairy Cows in Thousands'
+//           },
+//
+//           legend: {
+//               layout: 'horizontal',
+//               borderWidth: 0,
+//               backgroundColor: 'rgba(255,255,255,0.85)',
+//               floating: true,
+//               verticalAlign: 'top',
+//               y: 25
+//           },
+//
+//           mapNavigation: {
+//               enabled: true
+//           },
+//
+//           colorAxis: {
+//               min: 1,
+//               type: 'logarithmic',
+//               minColor: '#EEEEFF',
+//               maxColor: '#000022',
+//               stops: [
+//                   [0, '#EFEFFF'],
+//                   [0.67, '#4444FF'],
+//                   [1, '#000022']
+//               ]
+//           },
+//
+//           series: [{
+//               animation: {
+//                   duration: 1000
+//               },
+//               data: data,
+//               mapData: Highcharts.maps['countries/us/us-all'],
+//               joinBy: ['postal-code', 'code'],
+//               dataLabels: {
+//                   enabled: true,
+//                   color: '#FFFFFF',
+//                   format: '{point.code}'
+//               },
+//               name: 'dairy cow population in thousands',
+//               tooltip: {
+//                   pointFormat: '{point.code}: {point.value}'
+//               }
+//           }]
+//       });
+//     });
