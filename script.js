4ca457edc0417c570b03f87df44cf173e367d52a
Rori Kotch <r.kotch1@umiami.edu>
2017-04-29 19:35:58 -0400

    Added a map

--- script.js
+++ script.js
@@ -136,3 +136,67 @@ Highcharts.chart("container", {
       }]
 
     });
+
+    /////
+    $.getJSON('http://cheeseproject.me/data.html?', function (data) {
+
+        // Make codes uppercase to match the map data
+        $.each(data, function () {
+            this.code = this.code.toUpperCase();
+        });
+
+        // Instanciate the map
+        Highcharts.mapChart('annoying', {
+
+            chart: {
+                borderWidth: 0
+            },
+
+            title: {
+                text: 'Cheese Producing States by Thousands of Cows'
+            },
+
+            legend: {
+                layout: 'horizontal',
+                borderWidth: 0,
+                backgroundColor: 'rgba(255,255,255,0.85)',
+                floating: true,
+                verticalAlign: 'top',
+                y: 25
+            },
+
+            mapNavigation: {
+                enabled: true
+            },
+
+            colorAxis: {
+                min: 1,
+                type: 'logarithmic',
+                minColor: '#EEEEFF',
+                maxColor: '#000022',
+                stops: [
+                    [0, '#EFEFFF'],
+                    [0.67, '#4444FF'],
+                    [1, '#000022']
+                ]
+            },
+
+            series: [{
+                animation: {
+                    duration: 1000
+                },
+                data: data,
+                mapData: Highcharts.maps['countries/us/us-all'],
+                joinBy: ['postal-code', 'code'],
+                dataLabels: {
+                    enabled: true,
+                    color: '#FFFFFF',
+                    format: '{point.code}'
+                },
+                name: 'Cows by Thousand',
+                tooltip: {
+                    pointFormat: '{point.code}: {point.value}/km²'
+                }
+            }]
+        });
+    });
