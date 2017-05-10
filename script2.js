bae42d668c9eda78d6c51d91aedbed57bb8311ae
Rori Kotch <r.kotch1@umiami.edu>
2017-05-10 16:07:23 -0400

    Took out legends on chart

--- script2.js
+++ script2.js
@@ -5,6 +5,11 @@ Highcharts.chart("containerfour", {
         text: "U.S. MILK PRODUCTION FROM 2010 TO 2017"
     },
 
+    legend: {
+    enabled: false
+},
+
+
     subtitle: {
         text: "This data was sourced from the U.S.D.A. Department of Agriculture"
     },
@@ -40,6 +45,10 @@ Highcharts.chart("chart", {
         text: "U.S. CHEESE PRODUCTION FROM JAN. 1990 TO JAN. 2017"
     },
 
+    legend: {
+    enabled: false
+},
+
     subtitle: {
         text: "This data was sourced from the U.S.D.A. Department of Agriculture"
     },
@@ -64,61 +73,6 @@ Highcharts.chart("chart", {
     },
 
     series: [{
-        name: "Production",
         data: [492980000, 501954000, 520389000, 517261000, 538029000, 561722000, 589951000, 598166000, 617245000, 636971000, 693003000, 693797000, 713939000, 709599000, 738086000, 754667000, 782370000, 828272000, 819441000, 823902000, 842519000, 883505000, 911773000, 931691000, 962429000, 994542000, 1003278000, 1044983000]
     }]
   });
-
-
-  // Get the modal
-var modal = document.getElementById('modal-one');
-
-// Get the button that opens the modal
-var btn = document.getElementById("button-one");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
